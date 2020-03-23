3e6da3ae755418b02df38d259ae63394d41f4e93
samer.xie <samer.xie@unisoc.com>
2020-03-23 09:41:47 +0800

    Bug #1284885 sched: set min cpu mask variable
    [root cause  ] set min cpu mask variable
    [changes     ] set min cpu mask variable
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sharkl6
    [test case]:scheduler
    [reviewers   ]
    [change_type ] feature
    [tag_product ] common
    
    min cpu mask is lettle core cpumask set:
    1. when task can run in idle little core, not need check energy diff,can
    return as target_cpu.
    2. limit rt task will be placed little core.
    3. for min cap cpumask,will set a bigger freq_margin to improve cpu frequency.
    
    Change-Id: I655b532bd477a9825606387669235590e357853d
    Signed-off-by: samer.xie <samer.xie@unisoc.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -94,6 +94,8 @@ __read_mostly int scheduler_running;
  */
 int sysctl_sched_rt_runtime = 950000;
 
+/*record the min capacity cpus*/
+struct cpumask min_cap_cpu_mask;
 
 /*
  * Serialization rules:
@@ -7740,6 +7742,8 @@ void __init sched_init(void)
 	init_uclamp();
 
 	scheduler_running = 1;
+
+	cpumask_clear(&min_cap_cpu_mask);
 }
 
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
