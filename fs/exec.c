74b1cb69398957aac5c09762992e6f9f93973bcd
Pzqqt <821026875@qq.com>
2025-01-22 09:49:50 +0800

    KernelSU: Allows manual hooking without using kprobes
    Some apps will detect that KernelSU uses kprobes and think that the device
    environment is abnormal. Therefore, to avoid being detected, it is best to
    follow the official documentation of KernelSU to manually hook, just like
    doing it on a traditional kernel.
    
    This commit allows us to manually hook while keeping `CONFIG_KPROBES` enabled.
    
    Notes:
    The kernel having kprobes support does not mean that the environment is
    abnormal, because GKI has kprobes support by default, and disabling kprobes
    support will make it impossible to start the device.
    
    Signed-off-by: Pzqqt <821026875@qq.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1870,6 +1870,13 @@ out_files:
 
 	return retval;
 }
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+                               void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+                                        void *argv, void *envp, int *flags);
+#endif
 
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
@@ -1878,6 +1885,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 {
 	struct linux_binprm *bprm;
 	int retval;
+
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
--- fs/open.c
+++ fs/open.c
@@ -324,6 +324,11 @@ int vfs_fallocate(struct file *file, int mode, loff_t offset, loff_t len)
 }
 EXPORT_SYMBOL_GPL(vfs_fallocate);
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+                                int *flags);
+#endif
+
 int ksys_fallocate(int fd, int mode, loff_t offset, loff_t len)
 {
 	struct fd f = fdget(fd);
@@ -402,6 +407,10 @@ static long do_faccessat(int dfd, const char __user *filename, int mode, int fla
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 	const struct cred *old_cred = NULL;
+
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
