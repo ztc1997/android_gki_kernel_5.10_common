8887145a0f9072f0ba7c07246aa5e31a97009a28
Pzqqt <821026875@qq.com>
2025-03-13 08:35:37 +0800

    Revert "Revert "KernelSU: Allows manual hooking without using kprobes""
    https://github.com/tiann/KernelSU/commit/97b2411316c749d3b5385e8c054d206aa741ae31
    
    🤷🤷🤷
    
    This reverts commit c7e2963215a63c0c625cd8e9b33d2dec9e5fa75a.

--- drivers/staging/kernelsu/ksu.c
+++ drivers/staging/kernelsu/ksu.c
@@ -57,11 +57,9 @@ int __init kernelsu_init(void)
 
 	ksu_throne_tracker_init();
 
-#ifdef CONFIG_KPROBES
+#ifdef KSU_HOOK_WITH_KPROBES
 	ksu_sucompat_init();
 	ksu_ksud_init();
-#else
-	pr_alert("KPROBES is disabled, KernelSU may not work, please check https://kernelsu.org/guide/how-to-integrate-for-non-gki.html");
 #endif
 
 #ifdef MODULE
@@ -80,7 +78,7 @@ void kernelsu_exit(void)
 
 	destroy_workqueue(ksu_workqueue);
 
-#ifdef CONFIG_KPROBES
+#ifdef KSU_HOOK_WITH_KPROBES
 	ksu_ksud_exit();
 	ksu_sucompat_exit();
 #endif
