52244633c46baac3d60bb247a12978b36a557b13
Pzqqt <821026875@qq.com>
2025-06-08 15:03:29 +0800

    [SQUASH] Import susfs v1.5.7
    https://gitlab.com/simonpunk/susfs4ksu/-/tree/5a3153f9f8b18ed81628d9cc33726f52e5a5f5c6

--- drivers/staging/kernelsu/apk_sign.c
+++ drivers/staging/kernelsu/apk_sign.c
@@ -324,7 +324,7 @@ module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 
 #endif
 
-bool is_manager_apk(char *path)
+bool ksu_is_manager_apk(char *path)
 {
 	return check_v2_signature(path);
 }
