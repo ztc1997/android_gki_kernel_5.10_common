a0e14e4df1e884d96db6dbf169af8c22ea398ff3
Pzqqt <821026875@qq.com>
2025-06-28 14:57:33 +0800

    susfs: Completely remove sus-su support
    - We always do the required hooks for KernelSU manually. There is no dispute
      that manual hooks are always better than kprobes (less overhead, less
      likely to be detected).
    - It's really bad that there are two sets of hook schemes in the fs code.

--- fs/stat.c
+++ fs/stat.c
@@ -194,11 +194,6 @@ extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *fla
  * 0 will be returned on success, and a -ve error code if unsuccessful.
  */
 
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-extern bool susfs_is_sus_su_hooks_enabled __read_mostly;
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-#endif
-
 static int vfs_statx(int dfd, const char __user *filename, int flags,
 	      struct kstat *stat, u32 request_mask)
 {
@@ -207,12 +202,6 @@ static int vfs_statx(int dfd, const char __user *filename, int flags,
 	int error;
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 	struct mount *mnt;
-#endif
-
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-	if (likely(susfs_is_sus_su_hooks_enabled)) {
-		ksu_handle_stat(&dfd, &filename, &flags);
-	}
 #endif
 
 	if (flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT | AT_EMPTY_PATH |
